661724f64d8971ce330eba50346e41dbcdc7ab88
robertlc04 <robertolopezcrus04@gmail.com>
2024-08-02 14:51:11 -0400

    packages and index file

---
+++ index.js
@@ -0,0 +1,11 @@
+const process = require('process');
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(process.env.PORT || 3000);
